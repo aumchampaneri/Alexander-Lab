c1444112dd1a153db856abab27baf01e94af2216
Aum Champaneri <aum.champaneri@outlook.com>
2024-11-05 16:09:01 +0000

    Create a separate Kidney DataFrame to do analysis on

--- 20190927 NanoString Data/Analysis.py
+++ 20190927 NanoString Data/Analysis.py
@@ -1,15 +1,23 @@
+from matplotlib import axis
 import pandas as pd
 
 # Load the CSV file into a DataFrame
 file_path = '/Users/admin/Alexander-Lab/20190927 NanoString Data/Noramalized-data-counts.csv'
-data = pd.read_csv(file_path)
+raw_data = pd.read_csv(file_path)
 
-# Display the first few rows of the DataFrame
-print(data.head())
 
-# Calculate the average of the data in three columns
-columns_to_average = ['20190927_RQ-016723_Control-K-1_07.RCC', '20190927_RQ-016723_Control-K-2_08.RCC', '20190927_RQ-016723_Control-K-3_09.RCC']
-data['Average'] = data[columns_to_average].mean(axis=1)
+""" #Print the first 5 rows of the DataFrame
+print(raw_data.head())
+"""
 
-# Display the first few rows of the DataFrame with the new 'Average' column
-print(data.head())
+# Extract the columns corresponding to the kidney samples
+kidney_columns = ['Probe Name', '20190927_RQ-016723_Control-K-1_07.RCC', '20190927_RQ-016723_Control-K-2_08.RCC', '20190927_RQ-016723_Control-K-3_09.RCC', '20190927_RQ-016723_FHKO-K-1_10.RCC', '20190927_RQ-016723_FHKO-K-2_11.RCC', '20190927_RQ-016723_FHKO-K-2_11.RCC']
+
+# Extract the kidney data into a new DataFrame
+kidney_data = raw_data[kidney_columns]
+
+""" # Print the first 5 rows of the kidney data DataFrame
+print(kidney_data.head())
+"""
+
+
