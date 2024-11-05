288767c2bdcfd32e320f9455760919a183d63eda
Aum Champaneri <aum.champaneri@outlook.com>
2024-11-05 16:27:45 +0000

    Refactor data processing functions for improved efficiency

--- 20190927 NanoString Data/Kidney Samples Analysis.py
+++ 20190927 NanoString Data/Kidney Samples Analysis.py
@@ -1,8 +1,9 @@
+from appscript import k
 from matplotlib import axis
 import pandas as pd
 
 # Load the CSV file into a DataFrame
-file_path = '/Users/admin/Alexander-Lab/20190927 NanoString Data/Noramalized-data-counts.csv'
+file_path = '/Users/admin/Alexander-Lab/20190927 NanoString Data/Normalized-data-counts.csv'
 raw_data = pd.read_csv(file_path)
 
 
@@ -19,4 +20,25 @@ kidney_data = raw_data[kidney_columns]
 """ # Print the first 5 rows of the kidney data DataFrame
 print(kidney_data.head())
 """
+
+
+# Get rid of rows 0 and 1 -- Contains no data
+kidney_data = kidney_data.drop([0, 1])
+
+# Rename the 'Probe Name' column to 'Gene'
+kidney_data = kidney_data.rename(columns={'Probe Name': 'Gene'})
+
+# Rename the columns to remove the long file names
+kidney_data = kidney_data.rename(columns={'20190927_RQ-016723_Control-K-1_07.RCC': 'Control 1'})
+kidney_data = kidney_data.rename(columns={'20190927_RQ-016723_Control-K-2_08.RCC': 'Control 2'})
+kidney_data = kidney_data.rename(columns={'20190927_RQ-016723_Control-K-3_09.RCC': 'Control 3'})
+kidney_data = kidney_data.rename(columns={'20190927_RQ-016723_FHKO-K-1_10.RCC': 'FHKO 1'})
+kidney_data = kidney_data.rename(columns={'20190927_RQ-016723_FHKO-K-2_11.RCC': 'FHKO 2'})
+kidney_data = kidney_data.rename(columns={'20190927_RQ-016723_FHKO-K-2_11.RCC': 'FHKO 3'})
+
+# Reset the index of the DataFrame
+kidney_data = kidney_data.reset_index(drop=True)
+
+## Data is now cleaned and ready for analysis
+print(kidney_data.head())
 
